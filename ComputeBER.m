e96565bd948c8ab5f6489c56fbbcec9a9240f880
ELabbassalah56 <abassalah92@gmail.com>
2020-11-07 16:20:05 +0200

    Update EEE481_lab1

---
+++ ComputeBER.m
@@ -0,0 +1,15 @@
+function BER= ComputeBER(bit_seq,rec_bit_seq)
+%
+% Inputs:
+%   bit_seq:     The input bit sequence
+%   rec_bit_seq: The output bit sequence
+% Outputs:
+%   BER:         Computed BER
+%
+% This function takes the input and output bit sequences and computes the
+% BER
+
+%%% WRITE YOUR CODE HERE
+  diffBIT  = biterr(bit_seq,rec_bit_seq);
+ BER= (diffBIT/length(bit_seq))
+%%%
